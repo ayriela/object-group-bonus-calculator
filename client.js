04447d612862e86923ab5c681dea7240c4a37412
Ayriel Ash <aya37278@bethel.edu>
2019-10-01 19:46:04 -0500

    forgot to commit earlier, functions to create new employee and calculate bonus complete

--- client.js
+++ client.js
@@ -39,5 +39,58 @@ const employees = [
 
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
+for (i in employees){
+  console.log( newEmployeeData(employees[i]) );
+}
 
-console.log( employees );
+function newEmployeeData(employee){
+  //create object to be filled
+  let=updateEmployee={};
+  if (employee.reviewRating >= 3){
+    //set bonus percent constant
+    const bonusPer=calculateBonus(employee);
+    updateEmployee={
+        name: employee.name,
+        bonusPercent: bonusPer,
+        totalCompensation: Math.round(employee.annualSalary * (1 + bonusPer)),
+        totalBonus: Math.round(bonusPer * employee.annualSalary)
+    };
+   
+  } else{
+     updateEmployee = {
+        name: employee.name,
+        bonusPercent: 0,
+        totalCompensation: employee.annualSalary,
+        totalBonus: 0
+     };//set new object for employees with no bonus
+  }
+  return updateEmployee;
+}//end newEmployeeData
+
+function calculateBonus(employee){
+  //set initial bonus percentage based on rating
+  let bonusPercent= 0;
+  if(employee.reviewRating === 5){
+    bonusPercent = 0.1;
+  } else if (employee.reviewRating === 4){
+    bonusPercent = 0.06;
+  } else {
+    bonusPercent = 0.04;
+  }
+  //check if employee has 15 years and add 5 percent
+  if (employee.employeeNumber.length===4){
+    bonusPercent += 0.05;
+  } 
+  //check if the salary is already over 65000 and lose 1 percent
+  if (Number(employee.annualSalary) > 65000){
+    bonusPercent -= 0.01;
+  }
+  //check to see if it's fallen outside the boundaries of allowed bonuses
+  if (bonusPercent > 0.13){
+    bonusPercent= 0.13;
+  } else if (bonusPercent<0){
+    bonusPercent=0;
+  }
+  //for debugconsole.log(bonusPercent);
+  return bonusPercent;
+};//end calculateBonus
